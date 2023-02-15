5234f96d5020e868c62a36ba85336c09ce98eca6
JM <julian.magnabosco@gmail.com>
2023-02-15 15:12:10 -0300

    cambios minimos

--- ping_main.py
+++ ping_main.py
@@ -8,6 +8,7 @@
 # else:
 #   print (f"{hostname} is down!")
 
+import pickle
 from tkinter import *
 from  tkinter import ttk
 
@@ -17,36 +18,10 @@ class AppPing(Frame):
         self.master['bg'] = '#AC99F2'
         self.pack(expand=True,fill=BOTH)
 
-        """
-        tabla = ttk.Frame(self)
-        
-        tabla.grid_columnconfigure(1, weight=1)
-
-        data = [
-            # Nr. Name  Active
-            ["1",   "ST", "True"],
-            ["2",   "SO", "Falseasdasdads"],
-            ["3",   "SX", "True"],
-            ]
-
-        #define our column
-        cx,cy=0,0
-        for y in data:
-            label_y = LabelFrame(tabla)
-            
-            for x in y:
-                label_x = Label(label_y,text=x,width=len(x))
-                label_x.grid(row=0,column=cx)
-                cx+=1
-            label_y.grid(row=cy,column=0)
-            cy+=1
-        tabla.pack()
-        """
-        
         menu = LabelFrame(self,text="Opciones")
         menu.grid(column=2,row=0,sticky=S+N+E+W)
 
-        texto_label = Label(menu,text="Check Rate")
+        texto_label = Label(menu,text="Tasa de verificación")
         texto_label.pack(side=TOP)
         texto = Entry(menu)
         texto.pack(side=TOP,padx = 10, pady = 5)
@@ -54,31 +29,20 @@ class AppPing(Frame):
 
 
         tabla_scrolly = Scrollbar(self)
-        # tabla_scrolly.pack(side=RIGHT, fill=Y)
         tabla_scrolly.grid(column=1,row=0,sticky=N+S)
 
         tabla_scrollx = Scrollbar(self,orient='horizontal')
-        # tabla_scrollx.pack(side= BOTTOM,fill=X)
         tabla_scrollx.grid(column=0,row=1,sticky=W+E)
 
         agarre = ttk.Sizegrip(self)
-        # tabla_scrollx.pack(side= BOTTOM,fill=X)
         agarre.grid(column=2,row=1,sticky=S+E)
         
-        def select(a):
-            curItem = self.tabla.focus()
-            textItem = self.tabla.item(curItem)["values"]
-            print (textItem)
-            
-            newWindow = Toplevel(master)
-            newWindow.title("New Window")
-            Label(newWindow, text =textItem).pack()
+
 
         self.tabla = ttk.Treeview(self,yscrollcommand=tabla_scrolly.set, xscrollcommand =tabla_scrollx.set)
-        self.tabla.bind('<Double-Button-1>', select)
+        self.tabla.bind('<Double-Button-1>', self.select)
 
 
-        # self.table.pack(expand=True,fill=BOTH)
         self.tabla.grid(column=0,row=0,sticky=S+N+E+W)
         self.columnconfigure(0,weight=1)
         self.rowconfigure(0,weight=1)
@@ -88,24 +52,26 @@ class AppPing(Frame):
 
         #define our column
 
-        self.tabla['columns'] = ('player_id', 'player_name', 'player_Rank', 'player_states', 'player_city')
+        self.tabla['columns'] = ('nombre', 'ip', 'estado')
 
         # format our column
         self.tabla.column("#0", width=0,  stretch=NO)
         self.tabla.heading("#0",text="",anchor=W)
-        self.insertar_columna("player_id")
-        self.insertar_columna("player_name")
-        self.insertar_columna("player_Rank")
-        self.insertar_columna("player_states")
-        self.insertar_columna("player_city")
-
+        self.insertar_columna("nombre")
+        self.insertar_columna("ip")
+        self.insertar_columna("estado")
 
         #add data 
-        self.insertar_fila(('1','Ninja','101','Oklahoma', 'Moore'))
-        self.insertar_fila(('2','Ranger','102','Wisconsin', 'Green Bay'))
-        self.insertar_fila(('3','Deamon','103', 'California', 'Placentia'))
-        self.insertar_fila(('4','Dragon','104','New York' , 'White Plains'))
-        self.insertar_fila(('5','CrissCross','105','California', 'San Diego'))
+        self.check()
+
+    def select(self,*args):
+        curItem = self.tabla.focus()
+        textItem = self.tabla.item(curItem)["values"]
+        print (textItem)
+        
+        newWindow = Toplevel(self.master)
+        newWindow.title("New Window")
+        Label(newWindow, text =textItem).pack()
 
     def insertar_fila(self, valores):
         self.tabla.insert(parent='',index='end',text='',
@@ -114,11 +80,18 @@ class AppPing(Frame):
     def insertar_columna(self, texto):
         self.tabla.column(texto,anchor=W,width=80)
         self.tabla.heading(texto,text=texto,anchor=W)
+    
+    def check(self):
+        self.insertar_fila()
+
+    def quit(self):
+        Frame.quit(self)
+
 
 if __name__ == "__main__":
     app = AppPing()
     app.master.title('PythonGuides')
-    app.master.geometry('500x300')
+    app.master.geometry("800x500")
     app.mainloop()
     app.quit()
 
